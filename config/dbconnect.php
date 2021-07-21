97e23a567f1968fc72db2bf103b9a9b7108b3c6d
nikitha26 <mnikitha365@gmail.com>
2021-07-21 13:58:39 +0530

    some changes

--- config/dbconnect.php
+++ config/dbconnect.php
@@ -1,7 +1,20 @@
 <?php
+    //Locaol Database connection
+    // $host = "80";
+    // $db = "todo";
+    // $user = "nikitha";
+    // $pass = "";
+
+    //Remote Database Connection
+    $host = "remotemysql.com";
+    $db = "D1tswlUiRP";
+    $user = "D1tswlUiRP";
+    $pass = "vaMdQmCZMf";
+
+
 
     //Connection to database
-    $conn = mysqli_connect('localhost', 'nikitha', 'nikitha12345','todo app');
+    $conn = mysqli_connect($host,$user, $pass,$db);
 
     //Check connection
     if(!$conn){
